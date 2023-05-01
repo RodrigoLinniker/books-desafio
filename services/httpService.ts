356ba25e66f7605802db6f1158ec0e474332a286
RodrigoLCS <rodrigo_linnik123@hotmail.com>
2023-04-30 23:01:40 -0300

    first commit

--- services/httpService.ts
+++ services/httpService.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: process.env.BASE_URL || 'http://localhost:3333',
+  baseURL: process.env.BASE_URL || 'http://localhost:3000',
 });
 
 export default api;
